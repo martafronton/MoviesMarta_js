791ef396916ebcd2a957d4cee1d2d3a4c5ee43c5
martafronton <martafronton@gmail.com>
2025-10-05 00:21:36 +0200

    Mostrar recursos
    Mostrar los diferentes recursos a adivinar al pulsar el botón adivina.

--- assets/js/juego.js
+++ assets/js/juego.js
@@ -9,7 +9,7 @@ let index = 1
 const getElementsDeck = () => {
     let elementDeck = []
     for(let i = 1; i <= NMOVIES; i++) {
-        for(let j = 1; j <= NELEMENTSPMOVIE; j++) {
+        for(let j = 0; j < NELEMENTSPMOVIE; j++) {
             elementDeck.push("0"+i+"C"+j)
         } 
     }
@@ -19,25 +19,53 @@ const getElementsDeck = () => {
 }
 
 const getMovie = () => {
-    const movie = `0${index + 1}M`;
-    index++;
+    const movie = `0${index + 1}M`
+    index++
 
     if (index >= NMOVIES) {
-        index = 0;
+        index = 0
     }
 
-    return movie;
+    return movie
 };
 
+let elementDeck = getElementsDeck()
+
+const getElement = () => {
+    if(elementDeck.length === 0)
+        throw 'No hay más tarjetas'
+    const tarjeta = elementDeck.pop()
+    return tarjeta
+}
 
 
 
-let btNuevoJuego = document.getElementById('btNuevoJuego');
-let contenedorImagen = document.getElementById('pelicula-caratula');
 
+const btNuevoJuego = document.getElementById('btNuevoJuego')
+const contenedorImagen = document.getElementById('pelicula-caratula')
+const btAdivina = document.getElementById('btAdivina')
+const divElementos = document.querySelector("#elementos-pelicula")
 
 btNuevoJuego.addEventListener('click', function (event) {
     let movie = getMovie();
-    contenedorImagen.innerHTML = `<img class="elemento" src="assets/movies/${movie}.jpg" alt="Carátula">`;
+    contenedorImagen.innerHTML = `<img class="elemento" src="assets/movies/${movie}.jpg" alt="Carátula">`
+    divElementos.innerHTML = ""
+    elementDeck = getElementsDeck()
     event.stopPropagation();
-});
+})
+
+
+
+
+btAdivina.addEventListener('click', function (event) {
+    const elemento = getElement()
+
+    const div = document.createElement('div')
+    div.classList.add('elemento')
+    const img = document.createElement('img')
+    img.src = `assets/characters/${elemento}.jpg`
+    img.classList.add('recurso')
+
+    div.appendChild(img)
+    divElementos.appendChild(div)
+})
